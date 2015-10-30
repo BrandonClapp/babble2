fa9807de2e14dcc1f7e0decea5e54b831929a525
Brandon Clapp <clapp09@gmail.com>
2015-10-29 22:34:54 -0500

    support themes

--- client/config.js
+++ client/config.js
@@ -1,8 +1,8 @@
 module.exports = {
   ext: ['./app/styles/ext'],
   sass: [
-      './app/styles/sass/*.scss',
-      './app/styles/sass/*.sass'
+      './app/styles/sass/**/*.scss',
+      './app/styles/sass/**/*.sass'
   ],
   css: './app/styles/css/'
 }
