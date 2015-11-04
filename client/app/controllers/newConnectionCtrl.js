0be7960effeb84f45dc2d3ca731ebb2f5ee901a7
toannguyen83 <toannguyen83@gmail.com>
2015-11-03 23:12:24 -0600

    Set default server and port for easy testing

--- client/app/controllers/newConnectionCtrl.js
+++ client/app/controllers/newConnectionCtrl.js
@@ -2,9 +2,14 @@
   'use strict'
 
   angular.module('babble').controller('newConnectionCtrl', ['$scope', function($scope) {
+    $scope.form = {
+      host: '127.0.0.1',
+      port: 8888
+    };
 
     var close = function() {
-      $scope.form = {};
+      //$scope.form = {};
+      $scope.form.password = null; // keep all info except password for reconnect friendly
       $scope.display.newConnectionOverlay = false;
       $scope.$apply();
     }
